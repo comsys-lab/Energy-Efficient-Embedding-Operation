e775ed3a189b2721bf97a62f01868e3b8d80f186
sangun-choi <sangun_choi@korea.ac.kr>
2025-01-17 16:37:34 +0000

    scripts

---
+++ EVASim/run_sim_tf.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Original code: https://github.com/rishucoding/reproduce_MICRO24_GPU_DLRM_inference
+
+### outdir ### 
+OUT="results_ed_r_nt_lk_nb_bs"
+mkdir -p $OUT
+##############
+
+### dataset ###
+data_path_dir="$(pwd)/datasets/"
+dataset_list=("wikipedia/wikitext_10m.txt") # "wikipedia/20220301de.txt" "wikipedia/20220301en.txt"
+###############
+
+### simulation parameters ###
+MEM_CFG=$1 # spad_naive
+EMB_DIM=8192
+EMB_ROW=131072
+EMB_TBL=1
+EMB_POOL=131072
+EMBS="$EMB_DIM,$EMB_ROW,$EMB_TBL,$EMB_POOL"
+
+NUM_BATCH=1
+BS=1
+##############################
+
+### others ###
+PyGenTbl='import sys; rows,tables=sys.argv[1:3]; print("-".join([rows]*int(tables)))'
+OUTDIR="$(echo "$EMBS" | sed 's/,/_/g')"
+echo $OUTDIR
+OUTDIR="${OUT}/${OUTDIR}_${NUM_BATCH}_${BS}"
+echo $OUTDIR
+mkdir -p $OUTDIR
+##############
+
+for dataset in "${dataset_list[@]}"; do
+    DATA_GEN_PATH=$data_path_dir$dataset
+    OUTFILE=$(echo "$dataset" | sed 's/\//_/g')
+    OUTFILE="$OUTDIR/$OUTFILE"
+    for e in $EMBS; do
+        IFS=','; set -- $e; EMB_DIM=$1; EMB_ROW=$2; EMB_TBL=$3; EMB_LS=$4; unset IFS;
+        EMB_TBL=$(python -c "$PyGenTbl" "$EMB_ROW" "$EMB_TBL")
+        python3 src/simulator.py --num-batches $NUM_BATCH --batch-size $BS\
+            --lookups-per-sample $EMB_LS --arch-sparse-feature-size $EMB_DIM\
+            --arch-embedding-size $EMB_TBL --data-generation=$DATA_GEN_PATH --memory-config=$MEM_CFG | tee $(pwd)/${OUTFILE}_${MEM_CFG}.log
+    done
+done
